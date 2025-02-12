e5f156c0759252a75a65263dd63669942b352c46
BrB1998 <bkbrahme98@gamil.com>
2025-02-11 18:38:34 -0600

    Array-2 Completed

---
+++ problem1.py
@@ -0,0 +1,20 @@
+#Find All Numbers Disappeared in an Array
+#Time COmplexity: O(n)
+#Space Complexity: O(1)
+
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        result = []
+        for i in range(len(nums)):
+            idx = abs(nums[i])-1
+            
+            if nums[idx] > 0:
+                nums[idx] *= -1
+
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                result.append(i+1)
+            else:
+                nums[i] *= -1
+        return result
