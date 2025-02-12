d25fe36de134ffd5cb67b81ce7d1e3ed9abe46a4
BrB1998 <bkbrahme98@gamil.com>
2025-02-11 18:34:16 -0600

    Array-2 Completed

---
+++ problem2.java
@@ -0,0 +1,49 @@
+//#Game of Life
+//Time COmplexity: O(m*n)
+//Space Complexity: O(1)
+
+class Solution {
+    public void gameOfLife(int[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+
+        for (int i=0; i< m; i++){
+            for (int j=0; j<n; j++){
+                int countAlives = countAlive(board,i,j, m, n);
+                if(board[i][j] == 1 && (countAlives < 2 || countAlives > 3)){
+                    //alive
+                    board[i][j] = 2;
+                }
+                if(board[i][j] == 0 && countAlives == 3){
+                    board[i][j] = 3;
+                }
+            }
+        }
+
+        for (int i=0; i< m; i++){
+            for (int j=0; j<n; j++){
+                if(board[i][j] == 2){
+                    board[i][j] = 0;
+                }
+                if(board[i][j] == 3){
+                    board[i][j] = 1;
+                }
+            }
+            }
+    }
+
+    private int countAlive(int[][] board, int i, int j, int m, int n){
+        int [][] dirs = new int [][]{{0,1}, {0,-1}, {1,0}, {-1,0}, {-1,-1}, {-1,1}, {1,-1}, {1,1}};
+        int count = 0;
+        for(int [] dir: dirs){
+            int nr = i + dir[0];
+            int nc = j + dir[1];
+            //bounds check
+            if(nr>=0 && nr<m && nc>=0 && nc<n 
+                && (board[nr][nc] == 1 || board[nr][nc] == 2)){
+                count++;
+            }
+        }
+        return count;   
+    }
+}
